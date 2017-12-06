8d131f5eb43a0c7110873e97b10615d945a8a5c9
MaherSaleem <the-maher@live.com>
2017-12-06 21:15:03 +0200

    move document segments to a function

--- src/main/java/com/AQAS/passages_segmentation/Driver.java
+++ src/main/java/com/AQAS/passages_segmentation/Driver.java
@@ -8,64 +8,11 @@ public class Driver {
 
     public static void main(String[] args) {
 
-
-        String documentText = PassageSegmentation.getDocumentText(5);
-        String[] keyPhrases = PassageSegmentation.getQueryKeyPhrases("ما هو مرض السكري");
-
-        String documentSentences[] = documentText.split("[؟?!.]");
-
-
-        //just printing
-        for (String sentence : documentSentences) {
-            System.out.println(sentence);
-            System.out.println("=============================================");
-
-        }
-
-        int state = ConfigPS.STATE_END;
-        int sentencesSize = documentSentences.length;
-        int startIndex = 0;
-        int endIndex = 0;
-        ArrayList<String> segments = new ArrayList<>();
-        for (int i = 0; i < sentencesSize; i++) {
-            String si = documentSentences[i];
-            if (PassageSegmentation.hasKeyPhrases(si, keyPhrases) || i == sentencesSize - 1) {
-                if (i == sentencesSize - 1) {// case of last sentence in document
-                    state = ConfigPS.STATE_LAST_SENTENCE;
-                }
-                if (state == ConfigPS.STATE_END || state == ConfigPS.STATE_LAST_SENTENCE) {
-                    startIndex = endIndex;
-                    endIndex = i;
-                    if (!PassageSegmentation.isEnoughSegment(startIndex, endIndex) && state != ConfigPS.STATE_LAST_SENTENCE) {
-                        state = ConfigPS.STATE_NOT_ENOUGH;
-                        continue;
-                    }
-
-                } else if (state == ConfigPS.STATE_NOT_ENOUGH) {
-                    endIndex = i;
-                    state = ConfigPS.STATE_END;
-                }
-                String segmentString = "";
-                for (int j = startIndex; j < endIndex; j++) {
-                    segmentString += documentSentences[j] + ". ";
-                }
-
-                if (state == ConfigPS.STATE_LAST_SENTENCE) {
-                    int LastSegmentIndex = segments.size() - 1;
-                    segmentString += documentSentences[sentencesSize-1];
-                    segments.set(LastSegmentIndex, segments.get(LastSegmentIndex) + ". " +segmentString);
-                } else {
-                    segments.add(segmentString);
-                }
-            }
-
-        }
-
-        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        ArrayList<String> segments = PassageSegmentation.getDocumentSegments(5, "ما هي اعراض مرض السكري", null);
+        System.out.println("***************Starting printing the segments*************");
         for (String sentence : segments) {
             System.out.println(sentence);
             System.out.println("=============================================");
-
         }
     }
 }
--- src/main/java/com/AQAS/passages_segmentation/PassageSegmentation.java
+++ src/main/java/com/AQAS/passages_segmentation/PassageSegmentation.java
@@ -1,5 +1,6 @@
 package com.AQAS.passages_segmentation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PassageSegmentation {
@@ -219,6 +220,67 @@ public class PassageSegmentation {
         return Arrays.stream(KeyPhrases).parallel().anyMatch(sentence::contains);
     }
 
+
+    public static ArrayList<String> getDocumentSegments(int documentId, String questionText, String splitRegex) {
+
+        splitRegex = splitRegex != null ? splitRegex : "[؟?!.]";
+
+
+        String documentText = PassageSegmentation.getDocumentText(documentId);
+        String[] keyPhrases = PassageSegmentation.getQueryKeyPhrases(questionText);
+        String documentSentences[] = documentText.split(splitRegex);
+
+
+        //just printing
+        for (String sentence : documentSentences) {
+            System.out.println(sentence);
+            System.out.println("=============================================");
+
+        }
+
+        int state = ConfigPS.STATE_END;
+        int sentencesSize = documentSentences.length;
+        int startIndex = 0;
+        int endIndex = 0;
+        ArrayList<String> segments = new ArrayList<>();
+        for (int i = 0; i < sentencesSize; i++) {
+            String si = documentSentences[i];
+            if (PassageSegmentation.hasKeyPhrases(si, keyPhrases) || i == sentencesSize - 1) {
+                if (i == sentencesSize - 1) {// case of last sentence in document
+                    state = ConfigPS.STATE_LAST_SENTENCE;
+                }
+                if (state == ConfigPS.STATE_END || state == ConfigPS.STATE_LAST_SENTENCE) {
+                    startIndex = endIndex;
+                    endIndex = i;
+                    if (!PassageSegmentation.isEnoughSegment(startIndex, endIndex) && state != ConfigPS.STATE_LAST_SENTENCE) {
+                        state = ConfigPS.STATE_NOT_ENOUGH;
+                        continue;
+                    }
+
+                } else if (state == ConfigPS.STATE_NOT_ENOUGH) {
+                    endIndex = i;
+                    state = ConfigPS.STATE_END;
+                }
+                String segmentString = "";
+                for (int j = startIndex; j < endIndex; j++) {
+                    segmentString += documentSentences[j] + ". ";
+                }
+
+                if (state == ConfigPS.STATE_LAST_SENTENCE) {
+                    int LastSegmentIndex = segments.size() - 1;
+                    segmentString += documentSentences[sentencesSize - 1];
+                    segments.set(LastSegmentIndex, segments.get(LastSegmentIndex) + ". " + segmentString);
+                } else {
+                    segments.add(segmentString);
+                }
+            }
+
+        }
+
+
+
+        return segments;
+    }
 
 
     public static boolean isEnoughSegment(int startIndex, int endIndex){
